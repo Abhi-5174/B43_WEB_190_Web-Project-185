43dcfad582a5eb51407ba9ac34db3c9aa20e5d33
abhi-5174 <abhi8483999@gmail.com>
2025-03-24 12:45:57 +0530

    Fixed googles oauth20 callback

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,9 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback",
+      callbackURL: `${
+        process.env.BASE_URL || "http://localhost:3000"
+      }/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
